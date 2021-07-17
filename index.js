92922438e0940f45bf8bbfda176e77099543980d
Kinshuk <kinshuktaya644@gmail.com>
2021-07-17 20:22:03 +0530

    Add a route and a controller for home and render a static html file as view

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const app = express();
 
 const port = 3000;
 
+// use express router
+app.use("/", require("./routes"));
+
+// use ejs as view engine 
+app.set("view engine","ejs");
+app.set("views","./views");
+app.use(express.urlencoded({required: true, extended: true}));
+app.use(express.static('assets'));
+
 app.listen(port , function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
