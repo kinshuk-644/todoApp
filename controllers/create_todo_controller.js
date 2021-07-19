159a8bcc92de6c05bec2f6171bb764b7865ab583
Kinshuk <kinshuktaya644@gmail.com>
2021-07-19 16:07:31 +0530

    Complete the Add Task Functionality

--- controllers/create_todo_controller.js
+++ controllers/create_todo_controller.js
@@ -1,12 +1,20 @@
 const Todo = require("../models/todo");
+const format = require("date-fns/format");
 
 module.exports.create = function(req,res){
 
-    console.log(req.body.category);
+    var datee= '';
+
+    if(req.body.dueDate!=""){
+        var dueDate = req.body.dueDate;
+        var new_date = new Date(parseInt(dueDate.slice(0,4)),parseInt(dueDate.slice(5,7))-1,parseInt(dueDate.slice(8)));
+        datee = format(new_date, "MMM dd, yyyy");
+    }
+    
     Todo.create({
         description: req.body.description,
         category: req.body.category,
-        dueDate: req.body.dueDate
+        dueDate: datee
     } , function(err, newTodo){
         if(err){
             console.log("error in creating a todo");
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,17 @@
+const Todo = require("../models/todo");
+
 module.exports.home = function(req,res){
-    return res.render('home', {
-        title: "Home"
+
+    Todo.find({}, function(err, todos){
+        if(err){
+            console.log("Error in fetching todos from db");
+            return;
+        }
+
+        return res.render('home', {
+            title: "Home", 
+            todo_list: todos
+        });
     });
+    
 };
